f3768df60d2933d8914ae04dcda85b88c3d0673e
Ieuan Priede <priedeieuan@gmail.com>
2025-05-07 10:39:05 +0200

    set up database

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u+^ckx_2%%m(=_$w1if+*=hhkui%tjgz9+0&7&9+h!%drz+=80'
+SECRET_KEY = os.environ.setdefault("SECRET_KEY", "7g8gjkl")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
