a1f26a6689326ad390f4c3e7c376b2a22ff1e368
Ieuan Priede <priedeieuan@gmail.com>
2025-05-08 09:21:19 +0200

    Add first model'creating a profile' & add profiles/templates/index.html

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    # One-to-one link with Django's built-in User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Custom fields for the user's profile
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='profile_images/', default='default.jpg')
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
