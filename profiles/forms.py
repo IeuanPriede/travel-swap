18aff94eb4e30d584470dfb2baa92bfe19b8df6e
Ieuan Priede <priedeieuan@gmail.com>
2025-05-26 11:59:05 +0200

    Add content to About page and re-styled contact form

--- profiles/forms.py
+++ profiles/forms.py
@@ -123,7 +123,15 @@ ImageFormSet = modelformset_factory(
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    subject = forms.CharField(max_length=150, required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control'
+    }))
+    subject = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control', 'rows': 5
+    }))
