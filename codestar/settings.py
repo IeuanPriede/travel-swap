dc7c5fcad3155f9e9c7bf71c18ec7e16ed1a7bcc
Ieuan Priede <priedeieuan@gmail.com>
2025-05-27 10:07:26 +0200

    Add runtime logging to debug travel log issue

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 import sys
+import logging
+import logging.config
 if os.path.isfile("env.py"):
     import env
 
@@ -182,3 +184,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
