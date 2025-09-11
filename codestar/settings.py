4290db02fb78522601ce8c626c79c92f76b98fb1
Ieuan Priede <priedeieuan@gmail.com>
2025-09-11 16:32:31 +0200

    Use WhiteNoise Manifest storage, remove static/admin, pin Python

--- codestar/settings.py
+++ codestar/settings.py
@@ -199,8 +199,7 @@ if 'test' in sys.argv:
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
